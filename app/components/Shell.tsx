db1190c3b1361fff1cbc519a75ca136f13e6c4ed
José Costa <j.costa.3394@gmail.com>
2026-06-24 11:50:52 +0200

    Anchor footer on short pages

--- app/components/Shell.tsx
+++ app/components/Shell.tsx
@@ -150,7 +150,7 @@ export function Shell({ children }: { children: React.ReactNode }) {
   }, [menuOpen]);
 
   return (
-    <div className="flex min-h-full flex-col">
+    <div className="flex min-h-screen flex-col">
       <a
         href="#main-content"
         className="sr-only focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50 focus:rounded-sm focus:bg-corpus-yellow focus:px-4 focus:py-2 focus:text-black"
